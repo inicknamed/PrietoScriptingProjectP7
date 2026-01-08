7f155205af2350ff3443e06c72b87aa7920bcd14
Damien <756225@student.dusd.net>
2026-01-08 11:44:06 -0800

    Added Start and Awake Scene
    fdsfsdfsd

---
+++ P7PrietoScriptingProject/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Update called");
+    }
+
+
+    void Awake()
+    {
+        Debug.Log("Awake called");
+    }
+
+    void Update()
+    {
+        
+    }
+}
