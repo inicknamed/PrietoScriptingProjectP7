84c1a5b779ed413b61c04a4e24126c81ad99107b
Damien <756225@student.dusd.net>
2026-03-11 11:32:24 -0700

    Finished Get Component

---
+++ P7PrietoScriptingProject/Assets/Scripts/GetComponent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetComponent : MonoBehaviour
+{
+    public GameObject otherGameObject;
+
+
+    private AnotherScript anotherScript;
+    private YetAnotherScript yetAnotherScript;
+    private BoxCollider boxCol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCol.size = new Vector3(3, 3, 3);
+        Debug.Log("The player's score is " + anotherScript.playerScore);
+        Debug.Log("The player has died " + yetAnotherScript.numberOfPlayerDeaths + " times");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Awake()
+    {
+        anotherScript = GetComponent<AnotherScript>();
+        yetAnotherScript = otherGameObject.GetComponent<YetAnotherScript>();
+        boxCol = otherGameObject.GetComponent<BoxCollider>();
+    }
+}
