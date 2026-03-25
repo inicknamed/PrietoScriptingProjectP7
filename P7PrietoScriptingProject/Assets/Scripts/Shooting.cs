99dd3353be3c34ca20c552fa05ab7126a81b24c1
Damien <756225@student.dusd.net>
2026-03-25 11:19:16 -0700

    676767

---
+++ P7PrietoScriptingProject/Assets/Scripts/Shooting.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Rigidbody bulletPrefab;
+    public Transform firePosition;
+    public float bulletSpeed;
+
+
+    private Inventory inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        inventory = GetComponent<Inventory>();
+    }
+
+
+    void Update()
+    {
+        Shoot();
+    }
+
+
+    void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && inventory.myStuff.bullets > 0)
+        {
+            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            bulletInstance.AddForce(firePosition.forward * bulletSpeed);
+            inventory.myStuff.bullets--;
+        }
+    }
+}
